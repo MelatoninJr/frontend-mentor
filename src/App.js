4ae1a804fa7e47abd9df93e3f45e86a6babfb825
MelatoninJr <dillon32300@gmail.com>
2023-01-19 19:36:44 -0600

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Desktop1 from './assets/bg-main-desktop.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home-container">
+      <div className="container-1">
+        <img src={Desktop1} alt="space-background-desktop" />
+      </div>
+      <div className="container-2">
+        <div className="form-container">
+          <form action="submit">
+            <input type="text" className='generic-input'placeholder='e.g Jane Appleseed'/>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
